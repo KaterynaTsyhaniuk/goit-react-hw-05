e2d30d9382cae7a063a19184aab8da746e04eee8
Kateryna Tsyhaniuk <kyzkakatryska@gmail.com>
2024-11-21 15:32:53 +0200

    add another info from movie

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -6,11 +6,22 @@ const MovieDetailsPage = () => {
   const { movieId } = useParams();
 
   const [movie, setMovie] = useState(null);
+  const [releaseYear, setReleaseYear] = useState("");
+  const [movieRating, setMovieRating] = useState("");
 
   useEffect(() => {
     const getData = async () => {
       const data = await searchMoviesById(movieId);
       setMovie(data);
+
+      if (data.release_date) {
+        setReleaseYear(data.release_date.split("-")[0]);
+      }
+
+      if (data.vote_average) {
+        const percent = Math.round((data.vote_average / 10) * 100);
+        setMovieRating(percent);
+      }
     };
     getData();
   }, [movieId]);
@@ -18,10 +29,21 @@ const MovieDetailsPage = () => {
   if (!movie) {
     return <h2>Loading ...</h2>;
   }
+
+  const genreName = movie.genres.map((genre) => genre.name).join(", ");
+
   return (
     <div>
       <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-      <h2>{movie.title}</h2>
+      <h2>
+        {movie.title}({releaseYear})
+      </h2>
+      <p>User Score: {movieRating}%</p>
+      <h3>Overview</h3>
+      <p>{movie.overview}</p>
+      <h4>Genres</h4>
+      <p>{genreName}</p>
+      <p>{movie.genre_ids}</p>
       <Link to="cast">MovieCast</Link>
       <Link to="reviews">MovieReviews</Link>
       <Outlet />
